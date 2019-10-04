8231861018c5daca437539e1f088553303089342
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-03 23:14:01 -0400

    Ruta para enviar los emails

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const emailSender = require("./utils/emailSender");
+const dotenv = require("dotenv");
+
+dotenv.config({path: "./config.env"})
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +12,32 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cors());
+
+app.post("/", async (req, res) => {
+  try {
+    const nombre = req.body.nombre;
+    const email = req.body.email;
+    const mensaje = req.body.mensaje;
+
+    if(!nombre || !email || !mensaje) {
+      res.status(400).json({
+        message: "Completar todos los campos"
+      })
+    }
+
+    await emailSender({nombre, email, mensaje});
+    
+    res.status(200).json({
+      message: "Email enviado con éxito"
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+      message: "Server error"
+    })
+  }
+})
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
