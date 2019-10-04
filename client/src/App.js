ff59614d70130f412aff4bd01f0e93a96f357a84
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-03 22:04:03 -0400

    Agregado redux-thunk

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import About from "./pages/About/About";
 import Skills from "./pages/Skills/Skills";
 import Projects from "./pages/Projects/Projects";
 import Gallery from "./pages/Gallery/Gallery";
+import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
 //Configuración de redux
 import {Provider} from "react-redux";
-import {createStore, compose} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import reducers from "./redux/reducers";
+import thunk from "redux-thunk";
+const middlewares = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers());
-
+const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares)));
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
         <Skills />
         <Projects />
         <Gallery />
+        <Contact />
         <Footer />
       </React.Fragment>
     </Provider>
