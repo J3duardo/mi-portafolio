bb4d2d0ed8aca07f3215a8ed60746fc7a0cc9652
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-10 15:24:55 -0400

    Agregada pestaña de Desarrollo Web a sección Habilidades

--- client/src/pages/Skills/Skills.jsx
+++ client/src/pages/Skills/Skills.jsx
@@ -24,6 +24,43 @@ const Skills = (props) => {
     });
   }
 
+  //Generar las pestañas
+  const createTabs = () => {
+    const tabsContent = [
+      {
+        id: "1",
+        content: "Fotografía"
+      },
+      {
+        id: "2",
+        content: "Retoque Fotográfico"
+      },
+      {
+        id: "3",
+        content: "Motion Graphics"
+      },
+      {
+        id: "4",
+        content: "Desarrollo Web"
+      }
+    ];
+
+    const tabs = tabsContent.map(tab => {
+      return (
+        <h3
+          key={tab.id}
+          id={tab.id}
+          onClick={(e) => setActiveTab(e)}
+          className={`skills__tab tab-${tab.id} ${activeContent === tab.id && "skills__active-tab"}`}
+        >
+          {tab.content}
+        </h3>
+      )
+    });
+
+    return tabs;
+  }
+
   //Mostrar la pestaña activa
   const setActiveTab = (e) => {
     const tabIndex = e.target.id;
@@ -59,27 +96,7 @@ const Skills = (props) => {
 
         {/* Pestañas para mostrar el contenido */}
         <div className="skills__tabs">
-          <h3
-            id="1"
-            onClick={(e) => setActiveTab(e)}
-            className={`skills__tab tab-1 ${activeContent === "1" && "skills__active-tab"}`}
-          >
-            Fotografía
-          </h3>
-          <h3
-            id="2"
-            onClick={(e) => setActiveTab(e)}
-            className={`skills__tab tab-2 ${activeContent === "2" && "skills__active-tab"}`}
-          >
-            Retoque Fotográfico
-          </h3>
-          <h3
-            id="3"
-            onClick={(e) => setActiveTab(e)}
-            className={`skills__tab tab-3 ${activeContent === "3" && "skills__active-tab"}`}
-          >
-            Motion Graphics
-          </h3>
+          {createTabs()}
         </div>
 
         {/* Fotografía */}
@@ -102,7 +119,12 @@ const Skills = (props) => {
                 alt="right arrow icon"
               />
             </div>
-            <p className="img-number">{`${activeImg}/${slides.length}`}</p>
+            <p 
+              className="img-number"
+              style={{display: `${activeContent === "1" ? "block" : "none"}`}}
+            >
+              {`${activeImg}/${slides.length}`}
+            </p>
             <div className="skills__img-slider" id="slider">
               {renderSlides()}
             </div>
@@ -131,6 +153,73 @@ const Skills = (props) => {
               className="skills__video"
             >
             </video>
+          </div>
+
+          {/* Desarrollo web */}
+          <div className={`skills__img ${activeContent === "4" && "skills__active-img"}`}>
+            <h3 className="skills__skill-title skills__skill-title--2">Tecnologías Web</h3>
+            <div className="skills__img-web-icons-container">
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/react.png" alt="react icon"/>
+                <p>ReactJS</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "90%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>90%</span>
+                </div>
+              </div>
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/nodejs.png" alt="nodejs icon"/>
+                <p>NodeJs</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "80%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>80%</span>
+                </div>
+              </div>
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/mongodb.png" alt="mongodb icon"/>
+                <p>MogoDB</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "75%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>75%</span>
+                </div>
+              </div>
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/css3.png" alt="css3 icon"/>
+                <p>CSS3</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "95%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>95%</span>
+                </div>
+              </div>
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/sass.png" alt="sass icon"/>
+                <p>Sass</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "90%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>90%</span>
+                </div>
+              </div>
+              <div className="skills__img-web-icon">
+                <img src="/img/dev-icons/javascript.png" alt="javascript icon"/>
+                <p>JavaScript</p>
+                <div className="skills__skill-level">
+                  <div className="skills__skill-level-bar">
+                    <div style={{width: "80%"}} className="skills__skill-level-bar-fill"></div>
+                  </div>
+                  <span>80%</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </article>
